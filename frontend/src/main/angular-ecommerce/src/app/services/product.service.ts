f939f0f354e817be38df2509c8b1a735c877c396
mz706521 <Faizan.Siddiqui@sprint.com>
2020-06-14 13:17:18 -0500

    Changing the backend url for prod

--- frontend/src/main/angular-ecommerce/src/app/services/product.service.ts
+++ frontend/src/main/angular-ecommerce/src/app/services/product.service.ts
@@ -10,8 +10,8 @@ import { ProductCategory } from '../common/product-category';
 })
 export class ProductService {
     
-  private baseUrl = 'http://localhost:8080/api/products';
-  private categoryUrl = 'http://localhost:8080/api/product-category';
+  private baseUrl = '/api/products';
+  private categoryUrl = '/api/product-category';
 
   constructor(private httpClient: HttpClient) { }
 
